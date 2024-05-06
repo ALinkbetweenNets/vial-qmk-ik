ee0eb3801535765d54af52626b750a719b86b479
A Link between Nets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-05-06 10:37:08 +0200

    Update rules.mk to enable mouse support and n-key rollover

--- keyboards/handwired/ianmaclarty/ik1_2/rules.mk
+++ keyboards/handwired/ianmaclarty/ik1_2/rules.mk
@@ -1,10 +1,10 @@
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 BOOTLOADER = rp2040
-MOUSEKEY_ENABLE = no        # Mouse keys
+MOUSEKEY_ENABLE = yes        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = no            # Enable N-Key Rollover
+NKRO_ENABLE = yes            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
